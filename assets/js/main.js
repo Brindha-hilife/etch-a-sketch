5e19a1e4c7d32126acc618949b351ff27cc61b6a
Brindha-hilife <bindu.hilifeai@gmail.com>
2022-06-28 13:24:50 +0530

    Add function for buttons index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,64 @@
 const container = document.querySelector('.container')
-const blackbtn = document.querySelector('.black');
+// const blackbtn = document.querySelector('.black');
 const color = document.querySelector('.color');
-const rgbbtn = document.querySelector('.rgb');
+const whitebtn = document.querySelector('.white');
 const sizeNum = document.querySelector('.size');
-const size = sizeNum.value;
-const erase = document.querySelector('.erase');
+let size = sizeNum.value;
+const erasebtn = document.querySelector('.erase');
 const resetbtn = document.querySelector('.reset');
 
-// create div using function
+// get the size
+sizeNum.addEventListener('click', () =>{
+    size = prompt("size");
+    container.innerHTML="";
+    createPixel(size);
+})
 
+// create div using function
 function createPixel(size){
+
     container.style.setProperty('--size', size);
     for (let i = 0; i < (size * size); i++){
         const div = document.createElement('div');
         div.style.backgroundColor = "#8080800f";
         div.classList.add('pixel');
-
-     
-        blackbtn.addEventListener('click', () =>{
-            div.addEventListener('mouseover', function(){
-                div.style.background = "black";
-            })
-            container.appendChild(div)
-        })
-    
-    
-        // rgbbtn.addEventListener('click', () =>{
-        //     div.addEventListener('mouseover', function(){
-        //         div.style.background = "black";
-        //     })
-        //     container.appendChild(div)
-        // })
-        // color.addEventListener('click', () =>{
-        //     div.addEventListener('mouseover', function(){
-        //         div.style.background = "black";
-        //     })
-        //     container.appendChild(div)
-        // })
-
-        // div.addEventListener('mouseover', function(){
-        //     div.style.backgroundColor = color.value;
-        
-        // })
-
-
         container.appendChild(div)
-        
+        //  black
+        // blackbtn.addEventListener('click', () =>{
+        //     div.addEventListener('mouseover', function(){
+        //         div.style.background = "black";
+        //     })
+        //     container.appendChild(div)
+        // })
+
+        //  white
+        whitebtn.addEventListener('click', () =>{
+            div.addEventListener('mouseover', function(){
+                div.style.background = "#fff ";
+            })
+        })
+
+        //  choice
+        color.addEventListener('click', () =>{
+            div.addEventListener('mouseover', function(){
+                div.style.background = color.value;
+            })
+
+        })
+
+        //  erase
+        erasebtn.addEventListener('click', () =>{
+            div.addEventListener('mouseover', function(){
+                div.style.background = "none";
+            })
+        })
+
+        // reset
+        resetbtn.addEventListener('click', () =>{
+            div.style.background = "none";
+        })
+       
     }
 }
-createPixel(size);
-
-function firstbutton(){
-    blackbtn.addEventListener('click', () =>{
-        div.addEventListener('mouseover', function(){
-            div.style.background = "black";
-        })
-        container.appendChild(div)
-    })
-}
-
- 
-firstbutton()
-
-
-
-
+createPixel(16);
 
